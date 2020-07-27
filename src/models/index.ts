f31ffb5a4c3b399a41b7ce3b7950442b69ddae33
tetraFour <a.korensky@mail.ru>
2020-07-28 02:45:57 +0300

    fix: remove extra imports

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,5 +1,3 @@
 export { default as UserModel } from './user.model';
-export { default as FileModel } from './file.model';
 export { default as MessageModel } from './message.model';
 export { default as MessageContentModel } from './messageContent.model';
-export { default as DialogModel } from './dialog.model';
